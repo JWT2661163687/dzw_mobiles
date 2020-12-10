447dbfdeccc498b4e3cf7fee1d45998695d7cf4e
jwt123 <2661163687@qq.com>
2020-12-10 11:14:15 +0800

    更新

--- js/axios_cfg.js
+++ js/axios_cfg.js
@@ -3,11 +3,11 @@ axios.defaults.withCredentials=true;
 //请求超时时间,毫秒
 axios.defaults.timeout=10000;
 //响应拦截器
-axios.interceptors.response.use(resp => {
+/*axios.interceptors.response.use(resp => {
 	if(resp.data.code == "300") {
 		location.href = "/dzw_mobiles/login.html";
 	}
 	return resp;
 }, error => {
 	Promise.reject(error);
-})
+})*/
